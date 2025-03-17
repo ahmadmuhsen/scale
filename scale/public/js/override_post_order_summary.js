74a8302ce07b6edbc332af1cc9cc6bbf0104fe78
Rama <ram20190153@std.psut.edu.jo>
2025-03-18 00:05:27 +0300

    fis org call

--- scale/public/js/override_post_order_summary.js
+++ scale/public/js/override_post_order_summary.js
@@ -8,13 +8,15 @@ function override_load_summary_of(doc, after_submission = false) {
 
         erpnext.PointOfSale.PastOrderSummary.prototype.load_summary_of = function (doc, after_submission = false) {
             debugger;
-            original_load_summary_of(doc, after_submission);
+            original_load_summary_of.call(this, doc, after_submission);
 
             this.events.new_order();
             this.toggle_component(false);
             this.$component.find(".no-summary-placeholder").css("display", "flex");
             this.$summary_wrapper.css("display", "none");
         }
+        
+        console.log("POS page loaded, custom load_summary_of function overridden");
     }
     else {
         setTimeout(override_load_summary_of, 200);
